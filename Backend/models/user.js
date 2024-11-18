e6e3014d72f95f2a5ac92a1af482d2105dc0a277
mzslav <149380586+mzslav@users.noreply.github.com>
2024-11-18 16:46:42 +0100

    +(POST) buyNFT

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -4,13 +4,11 @@ const UserSchema = new mongoose.Schema({
     
     username: {
         type: String,
-        required: true,
         unique: true,
     },
 
     email: {
         type: String,
-        required: false,
         unique: true,
     },
 
@@ -30,7 +28,7 @@ const UserSchema = new mongoose.Schema({
     purchaseLog: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Log',  
-        required: true,
+        
     },
 
     profilePicture:String,
