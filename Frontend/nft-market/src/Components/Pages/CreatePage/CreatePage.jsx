26d47738a698a7ce7ba770fd3456940b0567d836
mzslav <149380586+mzslav@users.noreply.github.com>
2024-12-10 20:10:42 +0100

    done create page

--- Frontend/nft-market/src/Components/Pages/CreatePage/CreatePage.jsx
+++ Frontend/nft-market/src/Components/Pages/CreatePage/CreatePage.jsx
@@ -1,12 +1,15 @@
 import Header from '../../Header/Header';
 import InputBlock from './Components/InputBlock/InputBlock';
+import Footer from '../../Footer/Footer';
+
 
 
 const Create = () => {
     return (
         <>
           <Header />
-          <InputBlock />
+          <InputBlock />  
+          <Footer />
           
             
 
